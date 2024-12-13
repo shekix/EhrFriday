eb3c44ceb32ceeddcc62c7dc2c6826e40a04bfea
shekix <105061972+shekix@users.noreply.github.com>
2024-12-13 18:29:31 +0530

    Add files via upload

---
+++ ehr queries.sql
@@ -0,0 +1,65 @@
+insert into UserType (Name) values
+('Provider'),
+('Patient')
+
+insert into Genders (Type) values
+('Male'),
+('Female'),
+('Other')
+
+insert into BloodGroups(Type)values
+('A+'),
+('A-'),
+('B+'),
+('B-'),
+('O+'),
+('O-'),
+('AB+'),
+('AB-')
+
+
+insert into Qualifications (Code,Name) values
+('MBBS','Bachelor of Medicine and Bachelor of Surgery'),
+('MD','Doctor of Medicine'),
+('DO','Doctor of Osteopathic Medicine'),
+('BM','Bachelor of Medicine'),
+('MS','Master of Surgery'),
+('MCh','Master of Chirurgiae'),
+('DM','Bachelor of Medicine and Bachelor of Surgery'),
+('DNB','Diplomate of National Board'),
+('MMed','Master of Medicine')
+
+
+INSERT INTO Specializations (Name) VALUES
+('Allergist/immunologist'),
+('Anesthesiologist'),
+('Cardiologist'),
+('Dermatologist'),
+('Endocrinologist'),
+('Family physician'),
+('Gastroenterologist'),
+('Geneticist'),
+('Hematologist'),
+('Hospice and palliative medicine specialist'),
+('Infectious disease physician'),
+('Internal Medicine'),
+('Nephrologist'),
+('Neurologist'),
+('Obstetrician/gynecologist (OBGYNs)'),
+('Oncologist'),
+('Ophthalmologist'),
+('Orthopedist'),
+('Otolaryngologist'),
+('Osteopath'),
+('Pathologist'),
+('Pediatrician'),
+('Physician executive'),
+('Plastic surgeon'),
+('Podiatrist'),
+('Psychiatrist'),
+('Pulmonologist'),
+('Radiologist'),
+('Rheumatologist'),
+('Sleep medicine specialist'),
+('Surgeon'),
+('Urologist');
